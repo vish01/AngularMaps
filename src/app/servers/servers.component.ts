4df12946952fea4237fbf265e3b4fa83fdfd6b7a
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2018-04-15 13:24:06 -0400

    update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,13 +5,12 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-servers',
   selector: 'app-servers',
   template: `
-    <app-server></app-server>
     <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {   }
 
   ngOnInit() {
   }
