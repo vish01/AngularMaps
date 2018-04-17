8d47d56790ab360977fba43baaf0582514511cae
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2018-04-16 22:52:30 -0400

    update css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title1: string = 'Fetching Google Maps API';
   title2: string ='Two Way Data Binding';
   lat: number = 41.637639;
   lng: number = -70.9290092;
 
-  itemCount: number = 2;
+  itemCount: number = 4;
   btnText: string = 'Add an Item';
   goalText: string = 'My first life goal';
-  
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
   }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+
 }
